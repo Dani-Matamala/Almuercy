bcf257d698a2749dbc428c0828b31de692086f19
Matamala Daniel <daniel20mat@gmail.com>
2022-01-10 12:13:50 -0300

    Se configuro el CRUD de la api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const meals = require('./routes/meals')
+const orders = require('./routes/orders')
+
 //creando el servidor con express
 const app = express()
 
@@ -11,8 +14,6 @@ app.use(bodyParser.json())
 app.use(cors())
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.get('*', (req, res) => {
-    res.send("chanchito Feliz")
-})
-
+app.use('api/meals', meals)
+app.use('/api/orders', orders)
 module.exports = app;
