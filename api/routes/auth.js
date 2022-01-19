0e9aafce73ffca0e1d2deaea832ea5f350d537ce
Matamala Daniel <daniel20mat@gmail.com>
2022-01-19 11:46:15 -0300

    endpoint creado para la creacion de usuarios en la base de datos

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,31 @@
 //Manejador de los pedidos de las comidas
-
 const express = require('express')
-const { send } = require('express/lib/response')
+const crypto = require('crypto')
 const Users = require('../models/User')
 
 const router = express.Router()
 
 router.post('/register', (req, res) => {
-    res.send('soy registro')
+    const { email, password } = req.body
+    crypto.randomByte(16, (err, salt) => {
+        const newSalt = salt.toString('base64')
+        crypto.pbkdf2(password, newSalt, 100, 64, sha1, (err, key) => {
+            const encrytedPassword = key.toString('base64')
+            Users.findOne({email}).exec()
+                .then(user => {
+                    if(user){
+                        return res.send('usario ya existe')
+                    }
+                    Users.create({
+                        email,
+                        password: encrytedPassword,
+                        salt: newSalt,
+                    }).then(() => {
+                        res.send('usuario creado con éxito')
+                    })
+                })
+        })
+    })
 })
 
 router.post('/login', (req, res) => {
